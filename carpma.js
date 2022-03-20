c19acd7a65869e9b28ac5e067d11d3b2dfd8fc31
Bektas SARI <94165627+bektas-sari@users.noreply.github.com>
2022-03-20 19:38:24 +0300

    Update carpma.js
    değişiklik yapıldı

--- carpma.js
+++ carpma.js
@@ -1,8 +1,9 @@
+// değişiklikler yapıldı
 const sayi1 = document.getElementById('sayi1');
-const sayi2 = document.getElementById('sayi2');
+const sayi2 = document.getElementById('sayi3');
 
 const sonuc = document.getElementById('sonuc');
 
 function carp(){
-	sonuc.innerHTML = sayi1.value * sayi2.value;
+	sonuc.innerHTML = sayi1.value * sayi3.value;
 }
